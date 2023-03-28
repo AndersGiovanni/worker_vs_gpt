15964f8e704a2330c9be28fbbf096a8567099507
hrmussa <andersgiovanni@gmail.com>
2023-03-28 15:04:20 +0200

    Add label matcher

--- src/worker_vs_gpt/utils.py
+++ src/worker_vs_gpt/utils.py
@@ -29,3 +29,35 @@ def save_json(path: Path, container: Iterable) -> None:
     print(f"Saving json to {path}")
     with open(path, "w") as outfile:
         json.dump(container, outfile, ensure_ascii=False, indent=4)
+
+
+class LabelMatcher:
+    """For label consistency in zero-shot classification."""
+
+    def __init__(self, labels: List[str], task: str = "ten-dim"):
+        self.labels = labels
+        self.task = task
+
+    def __call__(self, label: str, text: str) -> str:
+        if self.task == "ten-dim":
+            for true_label in self.labels:
+                if label.lower() in true_label.lower():
+                    return true_label
+            print(f"Label not found: {label}, for text: {text}")
+            return "neutral"
+
+        if self.task == "hate-speech":
+            if label.lower() == "off":
+                return "OFF"
+            print(f"Label not found: {label}, for text: {text}")
+            return "NOT"
+
+        if self.task == "sentiment":
+            if label.lower() == "positive":
+                return "positive"
+            elif label.lower() == "negative":
+                return "negative"
+            print(f"Label not found: {label}, for text: {text}")
+            return "neutral"
+
+        raise ValueError(f"Task not found: {self.task}")
