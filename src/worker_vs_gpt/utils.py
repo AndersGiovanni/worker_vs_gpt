fcfffc0cec4d3f6177f08c5da7b0ad9815ff41b8
jacdals97 <jacdals@hotmail.com>
2023-03-28 18:47:50 +0200

    augmentation script

--- src/worker_vs_gpt/utils.py
+++ src/worker_vs_gpt/utils.py
@@ -2,7 +2,10 @@ import json
 from pathlib import Path
 from typing import Dict, Iterable, List, Union
 import torch
+import pandas as pd
+from numpy import random
 
+rng = random.RandomState(42)
 
 def get_device() -> torch.device:
     """Get device."""
@@ -61,3 +64,45 @@ class LabelMatcher:
             return "neutral"
 
         raise ValueError(f"Task not found: {self.task}")
+
+
+def parse_output(input_string: str) -> List[str]:
+    input_list = input_string.split("\n")  # split the input by newline characters
+    output_list = []
+    for item in input_list:
+        item = item.lstrip(
+            "0123456789. "
+        )  # remove enumeration and any leading whitespace
+        if item:  # skip empty items
+            output_list.append(item)
+    return output_list
+
+
+def balanced_sample_df(df: pd.DataFrame, n: int) -> pd.DataFrame: 
+    class_counts = df["target"].value_counts()
+    min_samples_per_class = class_counts.min()
+    num_classes = class_counts.size
+    samples_per_class = n // num_classes
+
+    sampled_indices = []
+    for label in class_counts.index:
+        indices = df.index[df["target"] == label]
+        if class_counts[label] >= samples_per_class:
+            sampled_indices.extend(
+                rng.choice(indices, size=samples_per_class, replace=False)
+            )
+        else:
+            sampled_indices.extend(
+                rng.choice(indices, size=samples_per_class, replace=True)
+            )
+
+    df_sampled = df.loc[sampled_indices]
+    remaining_samples = n - df_sampled.shape[0]
+    if remaining_samples > 0:
+        remaining_indices = df.index.difference(df_sampled.index)
+        remaining_samples_df = df.loc[
+            rng.choice(remaining_indices, size=remaining_samples)
+        ]
+        df_sampled = pd.concat([df_sampled, remaining_samples_df])
+
+    return df_sampled.sample(frac=1, random_state = rng).reset_index(drop=True)
