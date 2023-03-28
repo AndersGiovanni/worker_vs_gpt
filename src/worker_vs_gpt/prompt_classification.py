db546b6d0b4dd7bee65b4c75f0230eed1a6f4944
hrmussa <andersgiovanni@gmail.com>
2023-03-28 09:34:55 +0200

    Remov target names from classification report

--- src/worker_vs_gpt/prompt_classification.py
+++ src/worker_vs_gpt/prompt_classification.py
@@ -90,9 +90,7 @@ def main(cfg: PromptConfig) -> None:
     accuracy = accuracy_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred, average="macro")
     # roc_auc = roc_auc_score(y_true, y_pred, average="macro", multi_class="ovo")
-    report = classification_report(
-        y_true=y_true, y_pred=y_pred, target_names=labels, output_dict=True
-    )
+    report = classification_report(y_true=y_true, y_pred=y_pred, output_dict=True)
     # report = classification_report(y_true, y_pred, labels=labels, output_dict=True)
 
     # Initialize wandb
