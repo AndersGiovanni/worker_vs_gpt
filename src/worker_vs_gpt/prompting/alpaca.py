7c1c5faca65d80864178b45267e9a05eb6a376a0
jacdals97 <jacdals@hotmail.com>
2023-04-04 14:33:35 +0200

    add alpaca support

---
+++ src/worker_vs_gpt/prompting/alpaca.py
@@ -0,0 +1,22 @@
+from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig, pipeline
+from langchain import PromptTemplate, LLMChain
+
+import torch
+
+tokenizer = LlamaTokenizer.from_pretrained("chavinlo/gpt4-x-alpaca")
+
+base_model = LlamaForCausalLM.from_pretrained(
+    "chavinlo/gpt4-x-alpaca",
+    load_in_8bit=True,
+    device_map='auto',
+)
+
+alpaca = pipeline(
+    "text-generation",
+    model=base_model, 
+    tokenizer=tokenizer, 
+    max_length=1024,
+    temperature=0.6,
+    top_p=0.95,
+    repetition_penalty=1.2
+)
