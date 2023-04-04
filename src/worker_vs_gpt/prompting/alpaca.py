b181123c7d1b3a107ce4186fe75a237f53011b43
jacdals97 <jacdals@hotmail.com>
2023-04-04 14:51:51 +0200

    alpaca support for analyse og tal

--- src/worker_vs_gpt/prompting/alpaca.py
+++ src/worker_vs_gpt/prompting/alpaca.py
@@ -3,20 +3,23 @@ from langchain import PromptTemplate, LLMChain
 
 import torch
 
-tokenizer = LlamaTokenizer.from_pretrained("chavinlo/gpt4-x-alpaca")
 
-base_model = LlamaForCausalLM.from_pretrained(
-    "chavinlo/gpt4-x-alpaca",
-    load_in_8bit=True,
-    device_map='auto',
-)
+def load_alpaca():
+    tokenizer = LlamaTokenizer.from_pretrained("chavinlo/gpt4-x-alpaca")
 
-alpaca = pipeline(
-    "text-generation",
-    model=base_model, 
-    tokenizer=tokenizer, 
-    max_length=1024,
-    temperature=0.6,
-    top_p=0.95,
-    repetition_penalty=1.2
-)
+    base_model = LlamaForCausalLM.from_pretrained(
+        "chavinlo/gpt4-x-alpaca",
+        load_in_8bit=True,
+        device_map='auto',
+    )
+
+    alpaca = pipeline(
+        "text-generation",
+        model=base_model, 
+        tokenizer=tokenizer, 
+        max_length=1024,
+        temperature=0.6,
+        top_p=0.95,
+        repetition_penalty=1.2
+    )
+    return alpaca
