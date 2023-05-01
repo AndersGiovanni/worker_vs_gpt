9077d0f1e758bd53068d6ce21fab739cc139f827
jacdals97 <jacdals@hotmail.com>
2023-05-01 09:47:38 +0200

    initial similarity

--- src/worker_vs_gpt/data_similarity.py
+++ src/worker_vs_gpt/data_similarity.py
@@ -15,13 +15,21 @@ from worker_vs_gpt.prompting.langchain_prompting import (
     DataTemplates,
 )
 from worker_vs_gpt.similarity.text_similarity import SentenceSimilarity
+from transformers import AutoTokenizer
+
+from worker_vs_gpt.data_processing import (
+    dataclass_hate_speech,
+    dataclass_sentiment,
+    dataclass_ten_dim,
+    dataclass_analyse_tal,
+)
 
 from worker_vs_gpt.config import (
     HATE_SPEECH_DATA_DIR,
     SENTIMENT_DATA_DIR,
     TEN_DIM_DATA_DIR,
     ANALYSE_TAL_DATA_DIR,
-    DATA_DIR,
+    SIMILARITY_DIR,
     SimilarityConfig,
 )
 
@@ -34,132 +42,119 @@ load_dotenv()
     config_name="config_similarity.yaml",
 )
 def main(cfg: SimilarityConfig) -> None:
-    # Load data
+    # can be 'analyse-tal', 'hate-speech', 'sentiment', 'ten-dim'
     if cfg.dataset == "analyse-tal":
-        text = "tweet"  # text column
-        language = "Danish"
-        base_dataset = pd.read_json(os.path.join(ANALYSE_TAL_DATA_DIR, "base.json"))
-        train_dataset = pd.read_json(os.path.join(ANALYSE_TAL_DATA_DIR, "train.json"))
+        dataset = dataclass_analyse_tal.AnalyseTalDataset(
+            ANALYSE_TAL_DATA_DIR / "train.json"
+        )
+        test_dataset = dataclass_analyse_tal.AnalyseTalDataset(
+            ANALYSE_TAL_DATA_DIR / "test.json"
+        )
+        base_dataset = dataclass_analyse_tal.AnalyseTalDataset(
+            ANALYSE_TAL_DATA_DIR / "base.json"
+        )
+        augmented_dataset = dataclass_analyse_tal.AnalyseTalDataset(
+            ANALYSE_TAL_DATA_DIR / f"{cfg.augmentation}_FAKE.json",
+            is_augmented=True,
+        )
     elif cfg.dataset == "hate-speech":
-        # read json
-        text = "tweet"  # text column
-        augmented = "augmented_tweet"
-        base_dataset = pd.read_json(os.path.join(HATE_SPEECH_DATA_DIR, "base.json"))
-        train_dataset = pd.read_json(os.path.join(HATE_SPEECH_DATA_DIR, "train.json"))
-        augmented_dataset = pd.read_json(
-            os.path.join(SENTIMENT_DATA_DIR, f"{cfg.augmentation}.json")
+        text = "tweet"
+        dataset = dataclass_hate_speech.HateSpeechDataset(
+            HATE_SPEECH_DATA_DIR / "train.json"
+        )
+        test_dataset = dataclass_hate_speech.HateSpeechDataset(
+            HATE_SPEECH_DATA_DIR / "test.json"
+        )
+        base_dataset = dataclass_hate_speech.HateSpeechDataset(
+            HATE_SPEECH_DATA_DIR / "base.json"
+        )
+        augmented_dataset = dataclass_hate_speech.HateSpeechDataset(
+            HATE_SPEECH_DATA_DIR / f"{cfg.augmentation}.json",
+            is_augmented=True,
         )
     elif cfg.dataset == "sentiment":
-        text = "text"  # text column
-        augmented = "augmented_text"
-        base_dataset = pd.read_json(os.path.join(SENTIMENT_DATA_DIR, "base.json"))
-        train_dataset = pd.read_json(os.path.join(SENTIMENT_DATA_DIR, "train.json"))
-        augmented_dataset = pd.read_json(
-            os.path.join(SENTIMENT_DATA_DIR, f"{cfg.augmentation}.json")
+        text = "text"
+        dataset = dataclass_sentiment.SentimentDataset(
+            SENTIMENT_DATA_DIR / "train.json"
+        )
+        test_dataset = dataclass_sentiment.SentimentDataset(
+            SENTIMENT_DATA_DIR / "test.json"
+        )
+        base_dataset = dataclass_sentiment.SentimentDataset(
+            SENTIMENT_DATA_DIR / "base.json"
+        )
+        augmented_dataset = dataclass_sentiment.SentimentDataset(
+            SENTIMENT_DATA_DIR / f"{cfg.augmentation}.json",
+            is_augmented=True,
         )
     elif cfg.dataset == "ten-dim":
-        text = "h_text"  # text column (can be text or h_text)
-        augmented = "augmented_h_text"
-        base_dataset = pd.read_json(os.path.join(TEN_DIM_DATA_DIR, "base.json"))
-        train_dataset = pd.read_json(os.path.join(TEN_DIM_DATA_DIR, "train.json"))
-        augmented_dataset = pd.read_json(
-            os.path.join(SENTIMENT_DATA_DIR, f"{cfg.augmentation}.json")
+        text = "h_text"
+        dataset = dataclass_ten_dim.SocialDataset(TEN_DIM_DATA_DIR / "train.json")
+        test_dataset = dataclass_ten_dim.SocialDataset(TEN_DIM_DATA_DIR / "test.json")
+        base_dataset = dataclass_ten_dim.SocialDataset(TEN_DIM_DATA_DIR / "base.json")
+        augmented_dataset = dataclass_ten_dim.SocialDataset(
+            TEN_DIM_DATA_DIR / f"{cfg.augmentation}.json",
+            is_augmented=True,
         )
     else:
-        raise ValueError(f"Dataset not found: {cfg.dataset}")
+        raise ValueError("Dataset not found")
+
+    dataset.data["test"] = test_dataset.data["train"]
+    dataset.data["base"] = base_dataset.data["train"]
+    dataset.data["original_train"] = dataset.data["train"]
+    dataset.data["augmented_train"] = augmented_dataset.data["train"]
+
+    dataset.preprocess(model_name=cfg.model)
+
+    base_dataset = dataset.data["base"].to_pandas()
+    train_dataset = dataset.data["original_train"].to_pandas()
+    augmented_dataset = dataset.data["augmented_train"].to_pandas()
+
+    del dataset
 
     Sentence_sim = SentenceSimilarity(cfg.model)
 
-    train_size = min(len(train_dataset), 5000)
-    train_dataset = train_dataset.head(train_size)
+    # Specify the length of train and validation set
+    validation_length = 750
+    if cfg.use_augmented_data:
+        text = "augmented_" + text
+        total_train_length = len(augmented_dataset[text])
+    else:
+        total_train_length = len(train_dataset[text]) - validation_length
 
-    # get all base examples
-    duplicated_text = augmented_dataset[text].unique()
+    # generate list of indices to slice from
+    indices = list(range(0, total_train_length, 500)) + [total_train_length]
 
-    # get within similarity between augmented examples generated from the same base example
-    augmented_dataset["within_base_augment_sim"] = 0
-    augmented_dataset["within_base_augment_sim"] = augmented_dataset.groupby(text)[
-        augmented
-    ].transform(lambda x: Sentence_sim.get_mean_similarity(x.values, x.values))
+    # Select only indices with value 5000 or less
+    indices = [4000]
+    res = {k: [] for k in ["size", "similarity"]}
 
-    # compute similarities between base examples and augmented examples
-    Sentence_sim.compute_sim_matrix(
-        augmented_dataset[text].values, augmented_dataset[augmented].values
-    )
+    for idx in indices:
+        if cfg.use_augmented_data:
+            if idx == 0:
+                continue
+            data = augmented_dataset.iloc[validation_length : idx + validation_length]
+        else:
+            data = pd.concat(
+                [
+                    base_dataset,
+                    train_dataset.iloc[validation_length : idx + validation_length],
+                ],
+            )
 
-    augmented_dataset[
-        "base_augment_pair_sim"
-    ] = (
-        Sentence_sim.get_similarity_pairs()
-    )  # pairwise similarity between base example and augmented example
-    augmented_dataset["base_augment_sim"] = Sentence_sim.get_similarity_sources_targets(
-        row=False
-    )  # mean similarity between augmented example and all base examples
+        Sentence_sim.prepare_features_labels(data[text].values, data["target"].values)
+        Sentence_sim.compute_TransRate()
 
-    # remove base examples from training set
-    train_oov_dataset = train_dataset[~train_dataset[text].isin(duplicated_text)]
+        Sentence_sim.compute_sim_matrix(data[text].values, data[text].values)
 
-    Sentence_sim.compute_sim_matrix(
-        train_oov_dataset[text].values, augmented_dataset[augmented].values
-    )
-    augmented_dataset[
-        "train_augment_sim"
-    ] = Sentence_sim.get_similarity_sources_targets(
-        row=False
-    )  # mean similarity between augmented example and all non-base training examples
+        res["size"].append(idx)
+        res["similarity"].append(Sentence_sim.get_similarity_sources_targets().mean())
 
-    Sentence_sim.compute_sim_matrix(
-        augmented_dataset[augmented].values, augmented_dataset[augmented].values
-    )
-    augmented_dataset[
-        "within_augment_sim"
-    ] = (
-        Sentence_sim.get_similarity_sources_targets()
-    )  # within similarity between all augmented examples
-
-    Sentence_sim.compute_sim_matrix(
-        train_dataset[text].values, train_dataset[text].values
-    )
-    train_dataset[
-        "within_train_sim"
-    ] = (
-        Sentence_sim.get_similarity_sources_targets()
-    )  # within similarity between train examples
-
-    # save mean and standard deviation of similarity scores in dictionary
-    similarity_dict = {
-        "within_base_augment_sim": {
-            "mean": augmented_dataset["within_base_augment_sim"].mean(),
-            "std": augmented_dataset["within_base_augment_sim"].std(),
-        },
-        "base_augment_pair_sim": {
-            "mean": augmented_dataset["base_augment_pair_sim"].mean(),
-            "std": augmented_dataset["base_augment_pair_sim"].std(),
-        },
-        "base_augment_sim": {
-            "mean": augmented_dataset["base_augment_sim"].mean(),
-            "std": augmented_dataset["base_augment_sim"].std(),
-        },
-        "train_augment_sim": {
-            "mean": augmented_dataset["train_augment_sim"].mean(),
-            "std": augmented_dataset["train_augment_sim"].std(),
-        },
-        "within_augment_sim": {
-            "mean": augmented_dataset["within_augment_sim"].mean(),
-            "std": augmented_dataset["within_augment_sim"].std(),
-        },
-        "within_train_sim": {
-            "mean": train_dataset["within_train_sim"].mean(),
-            "std": train_dataset["within_train_sim"].std(),
-        },
-    }
-    # turn similarity dictionary into dataframe
-    similarity_df = pd.DataFrame.from_dict(similarity_dict, orient="index")
-    # save similarity dataframe
-    similarity_df.to_csv(
+    # turn dict into dataframe and save as json
+    res = pd.DataFrame(res)
+    res.to_json(
         os.path.join(
-            DATA_DIR,
-            "similarity_results", f"{cfg.dataset}_{cfg.augmentation}_similarity.csv",
+            SIMILARITY_DIR, f"{cfg.dataset}_{cfg.augmentation}_similarity.json"
         )
     )
 
