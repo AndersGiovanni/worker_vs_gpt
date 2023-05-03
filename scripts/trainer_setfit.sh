539dbffb9bff802160c9626e2903ae08ec45c67f
hrmussa <andersgiovanni@gmail.com>
2023-05-03 15:38:42 +0200

    Add shellscript for setfit

---
+++ scripts/trainer_setfit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=experiments    # Job name
+#SBATCH --output=run_outputs/trainer_setfit.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --cpus-per-task=16       # Schedule one core
+#SBATCH --time=16:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu
+#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
+#SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
+
+hostname
+
+nvidia-smi
+
+module load poetry
+
+poetry shell
+
+python -m src.worker_vs_gpt.setfit_classification.py
