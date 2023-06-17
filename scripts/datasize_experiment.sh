69ce009ecd677809d17467c31148a0c5efc835cb
AGMoller <andersgiovanni@gmail.com>
2023-06-17 09:04:37 +0200

    Update shell scripts

--- scripts/datasize_experiment.sh
+++ scripts/datasize_experiment.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=experiments    # Job name
-#SBATCH --output=run_outputs/trainer_experiments.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --output=run_outputs/datasize_experiments.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=16       # Schedule one core
-#SBATCH --time=16:00:00          # Run time (hh:mm:ss) - run for one hour max
-#SBATCH --gres=gpu
+#SBATCH --time=12:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu:v100:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
+#SBATCH --account=researchers
 
 hostname
 
@@ -16,4 +17,4 @@ module load poetry
 
 poetry shell
 
-python -m src.worker_vs_gpt.__main__
+python -m src.worker_vs_gpt.datasize_experiment
--- scripts/normal_experiment.sh
+++ scripts/normal_experiment.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name=install_environment    # Job name
-#SBATCH --output=run_outputs/poetry_installer.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --job-name=experiments    # Job name
+#SBATCH --output=run_outputs/trainer_experiments.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=16       # Schedule one core
 #SBATCH --time=02:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu:v100:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 #SBATCH --account=researchers
 
 hostname
 
-module load poetry
+nvidia-smi
 
-poetry --version
+module load poetry
 
 poetry shell
 
-poetry update
-
-poetry install
+python -m src.worker_vs_gpt.__main__
