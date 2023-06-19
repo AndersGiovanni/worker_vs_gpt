dce68745581a83013032452a6737ca08620aa079
AGMoller <andersgiovanni@gmail.com>
2023-06-19 10:43:30 +0200

    change runtime for normal experiment

--- scripts/normal_experiment.sh
+++ scripts/normal_experiment.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=experiments    # Job name
 #SBATCH --output=run_outputs/trainer_experiments.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=16       # Schedule one core
-#SBATCH --time=02:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --time=06:00:00          # Run time (hh:mm:ss) - run for one hour max
 #SBATCH --gres=gpu:v100:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
