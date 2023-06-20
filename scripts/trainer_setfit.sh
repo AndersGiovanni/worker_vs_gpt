6d84aab62e1ea84541de1ae573b79967f91e894f
AGMoller <andersgiovanni@gmail.com>
2023-06-20 10:21:18 +0200

    Update setfit config

--- scripts/trainer_setfit.sh
+++ scripts/trainer_setfit.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=experiments    # Job name
 #SBATCH --output=run_outputs/trainer_setfit.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=12       # Schedule one core
-#SBATCH --time=16:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --time=1-00:00:00          # Run time (hh:mm:ss) - run for one hour max
 #SBATCH --gres=gpu
-#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
+#SBATCH --partition=red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 #SBATCH --account=researchers
 
