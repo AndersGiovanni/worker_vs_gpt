5b9ba4b0b387b0c2a98e8cc1ed9fb46a6f7abe4e
Anders Giovanni Møller <andersgiovanni@gmail.com>
2023-10-06 15:04:00 +0200

    Llama 2 classification

--- src/worker_vs_gpt/prompting/huggingface_prompting.py
+++ src/worker_vs_gpt/prompting/huggingface_prompting.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from transformers import AutoTokenizer
 from huggingface_hub import InferenceClient
-from worker_vs_gpt.config import HF_HUB_MODELS
+from worker_vs_gpt.config import HF_HUB_MODELS, SAMESIDE_DATA_DIR, CROWDFLOWER_DATA_DIR
 from worker_vs_gpt.prompting.datasets_config import (
     CrowdflowerConfig,
     SameSidePairsConfig,
 )
 import os
 
-from worker_vs_gpt.utils import parse_output
+from worker_vs_gpt.utils import few_shot_sampling, parse_output
 
 
 class HuggingfaceChatTemplate:
@@ -17,7 +17,7 @@ class HuggingfaceChatTemplate:
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.tokenizer.use_default_system_prompt = False
 
-    def get_template_classification(self, system_prompt: str, task: str):
+    def get_template_classification(self, system_prompt: str, task: str) -> str:
         chat = [
             {"role": "system", "content": system_prompt},
             {
@@ -45,3 +45,34 @@ class HuggingfaceChatTemplate:
         ]
 
         return self.tokenizer.apply_chat_template(chat, tokenize=False)
+
+
+if __name__ == "__main__":
+    config = CrowdflowerConfig
+
+    llm = InferenceClient(
+        model=HF_HUB_MODELS["llama-2-70b"],
+        token=os.environ["HUGGINGFACEHUB_API_TOKEN"],
+    )
+
+    template = HuggingfaceChatTemplate(
+        model_name=HF_HUB_MODELS["llama-2-70b"],
+    ).get_template_classification(
+        system_prompt=config.classification_system_prompt,
+        task=config.classification_task_prompt,
+    )
+
+    train = pd.read_json(os.path.join(CROWDFLOWER_DATA_DIR, "train.json"))
+    # few_shot_samples = few_shot_sampling(df=train, n=1, per_class_sampling=True)
+
+    output = llm.text_generation(
+        template.format(
+            few_shot="",
+            text="@meganmansyn Hahahaha! It's not horrible, if others were singing with I'm sure it could work. I wish I could afford my own drum set",
+        ),
+        max_new_tokens=25,
+        temperature=0.9,
+        repetition_penalty=1.2,
+    )
+
+    a = 1
